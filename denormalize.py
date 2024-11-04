0e9a48d794abda1e3d930d7fe975f78145af5b1e
csae8092 <p.andorfer@gmail.com>
2024-11-04 12:06:42 +0100

    refactoring

--- denormalize.py
+++ denormalize.py
@@ -2,9 +2,9 @@ import json
 import os
 
 import marko
-from jsonpath_ng import parse
 
 from config import DATA_FOLDER, DENORMALIZE_CONFIG
+from utils import add_view_labels, denormalize
 
 
 os.makedirs(DATA_FOLDER, exist_ok=True)
@@ -48,44 +48,5 @@ with open(fixme_file, "w", encoding="utf-8") as fp:
     json.dump(data, fp, ensure_ascii=False, indent=2)
 
 
-for x in DENORMALIZE_CONFIG:
-    print(x["table_label"])
-    final_file = os.path.join(*x["final_file"])
-    to_delete = x.get("fields_to_delete", [])
-    view_label_expression = x.get("label_lookup_expression", False)
-    for y in x["fields"]:
-        source_file = os.path.join(*y["source_file"])
-        with open(source_file, "r", encoding="utf-8") as f:
-            source_data = json.load(f)
-        print(f"  - {y['field_name']}")
-        seed_file = os.path.join(os.path.join(*y["seed_file"]))
-        with open(seed_file, "r", encoding="utf-8") as f:
-            seed_data = json.load(f)
-        for key, value in source_data.items():
-            old_values = value[y["field_name"]]
-            new_values = []
-            for old_val in old_values:
-                new_values.append(seed_data[f"{old_val['id']}"])
-            value[y["field_name"]] = new_values
-        print(f"  saving {x['table_label']} as {final_file}")
-        with open(final_file, "w", encoding="utf-8") as f:
-            json.dump(source_data, f, ensure_ascii=False, indent=2)
-
-
-print("now adding view_labels")
-for x in DENORMALIZE_CONFIG:
-    file_name = os.path.join(*x["final_file"])
-    try:
-        jsonpath_expr = parse(x["label_lookup_expression"])
-    except KeyError:
-        continue
-    print(f"    - {jsonpath_expr} for {file_name}")
-    with open(file_name, "r", encoding="utf-8") as fp:
-        data = json.load(fp)
-        for key, value in data.items():
-            try:
-                value["view_label"] = jsonpath_expr.find(value)[0].value
-            except IndexError:
-                value["view_label"] = f"NO MATCH FOR {x['label_lookup_expression']}"
-    with open(file_name, "w", encoding="utf-8") as fp:
-        json.dump(data, fp, ensure_ascii=False, indent=2)
+add_view_labels()
+denormalize()
---
+++ utils.py
@@ -0,0 +1,47 @@
+import os
+import json
+from jsonpath_ng import parse
+
+
+def denormalize(MODEL_CONFIG):
+    print("starting denormalizing")
+    for x in MODEL_CONFIG:
+        print(x["table_label"])
+        final_file = os.path.join(*x["final_file"])
+        for y in x["fields"]:
+            source_file = os.path.join(*y["source_file"])
+            with open(source_file, "r", encoding="utf-8") as f:
+                source_data = json.load(f)
+            print(f"  - {y['field_name']}")
+            seed_file = os.path.join(os.path.join(*y["seed_file"]))
+            with open(seed_file, "r", encoding="utf-8") as f:
+                seed_data = json.load(f)
+            for key, value in source_data.items():
+                old_values = value[y["field_name"]]
+                new_values = []
+                for old_val in old_values:
+                    new_values.append(seed_data[f"{old_val['id']}"])
+                value[y["field_name"]] = new_values
+            print(f"  saving {x['table_label']} as {final_file}")
+            with open(final_file, "w", encoding="utf-8") as f:
+                json.dump(source_data, f, ensure_ascii=False, indent=2)
+
+
+def add_view_labels(MODEL_CONFIG):
+    for x in MODEL_CONFIG:
+        print("now adding view labels")
+        file_name = os.path.join(*x["final_file"])
+        try:
+            jsonpath_expr = parse(x["label_lookup_expression"])
+        except KeyError:
+            continue
+        print(f"    - {jsonpath_expr} for {file_name}")
+        with open(file_name, "r", encoding="utf-8") as fp:
+            data = json.load(fp)
+            for key, value in data.items():
+                try:
+                    value["view_label"] = jsonpath_expr.find(value)[0].value
+                except IndexError:
+                    value["view_label"] = f"NO MATCH FOR {x['label_lookup_expression']}"
+        with open(file_name, "w", encoding="utf-8") as fp:
+            json.dump(data, fp, ensure_ascii=False, indent=2)

03bcf234e18d57a73caf699bfa6e6eeeee516028
csae8092 <p.andorfer@gmail.com>
2025-01-20 09:52:41 +0100

    potential confg-fix

--- config.py
+++ config.py
@@ -192,15 +192,20 @@ MODEL_CONFIG = [
                 "source_file": "ms_items",
                 "lookup_field": "cod_unit",
             },
-            {
-                "source_file": "cod_unit_placed",
-                "lookup_field": "cod_unit",
-            },
+            # {
+            #     "source_file": "cod_unit_placed",
+            #     "lookup_field": "cod_unit",
+            # },
         ],
         "fields": [
             {
                 "field_name": "quire",
                 "seed_file": [JSON_FOLDER, "quires.json"],
+                "source_file": [DATA_FOLDER, "cod_units.json"],
+            },
+            {
+                "field_name": "cod_unit_placed",
+                "seed_fiel": [JSON_FOLDER, "cod_unit_placed.json"],
                 "source_file": [DATA_FOLDER, "cod_units.json"],
             },
         ],
